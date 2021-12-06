22cb48b41a6a45612a9c1e5780ecf0a628cab332
denischoch <95230953+denischoch@users.noreply.github.com>
2021-12-06 16:42:49 +0000

    дд

--- p6_t4d.py
+++ p6_t4d.py
@@ -1,1 +1,18 @@
-
+import matplotlib.pyplot as plt
+n=11213
+x=[6]
+y=[3]
+l=3
+h=1
+n=10
+lastx=x[0]
+lasty=y[0]
+for i in range (n):
+  x.append(lastx+l)
+  y.append(lasty)
+  lastx+=l
+  x.append(lastx)
+  y.append(lasty+h)
+  lasty+=h
+plt.plot(x,y)
+plt.show()
